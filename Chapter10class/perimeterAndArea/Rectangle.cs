3f835c79abda5619f4510edb6038a430610e80a2
Nate <33699428+neilarika@users.noreply.github.com>
2018-03-27 17:39:42 -0500

    Add files via upload

---
+++ Chapter10class/perimeterAndArea/Rectangle.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+
+namespace Chapter10class
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        //constructors
+
+        //constructor with no parameters
+        public Rectangle(): this(1, 1)
+
+        {
+            //length = 1;
+            //Length = 1;
+            //Width = 1;
+        }
+
+        // constructor with  2 parameters
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+
+            //not as good
+            //we coulve said
+            //this.length = length;
+            //this.width = width;
+        }
+
+
+
+
+        //properties for length and width
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value >= 0 && value <= 20)
+                {
+                    length = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"length",
+                        value, "length must be between 0-20");
+                }
+            }
+        }
+
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value >= 0 && value <= 20)
+                {
+                    width = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"width",
+                        value, "width must be between 0-20");
+                }
+            }
+        }
+
+
+        public double Area
+        {
+            get
+            {
+                return length * width;
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return (length * 2) + (width * 2);
+            }
+        }
+
+        public override  String ToString()
+        {
+            //return "Length is " + Length;
+            return ($"Length: {Length} \nWidth: {Width} \n" +
+                $"Area: {Area:f2} \nPerimeter: {Perimeter:f2}");
+
+        }
+
+    }
+}
---
+++ Chapter10class/perimeterAndArea/testit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Chapter10class
+{
+    class testit
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //use the 2 parameter constructor
+                Rectangle rec = new Rectangle(12.6686534, 35.65464662);
+                Console.WriteLine(rec);
+                
+                
+                /*
+                //use the no-parameter constructor
+                Rectangle rec2 = new Rectangle(12.541543, 13.51484855);
+                rec2.Length = 5;
+                Console.WriteLine(rec2);
+
+                Console.WriteLine();
+
+                //object initializer
+                Rectangle rec3 = new Rectangle { Length = 13, Width = 5 };
+                Console.WriteLine(rec3);
+
+                Console.WriteLine();
+                */
+
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("You messsed up " + ex.Message);
+
+            }
+           
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
